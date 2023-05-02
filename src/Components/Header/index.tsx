4d0ecb2d29ef8fe69f1056ce187e2e94ac396a39
Faz-R <FazaR92@gmail.com>
2023-05-02 21:47:34 +0300

    feat: add top section for main page

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,6 +1,5 @@
-import { AppBar, Container, IconButton, SvgIcon, Toolbar } from '@mui/material';
+import { AppBar, Button, ButtonGroup, Container, IconButton, SvgIcon, Toolbar } from '@mui/material';
 import { ReactComponent as GraphIcon } from '../../assets/graphql.svg';
-import { AccountCircle } from '@mui/icons-material';
 
 const Header = () => {
   return (
@@ -8,7 +7,10 @@ const Header = () => {
       <Container>
         <Toolbar>
           <SvgIcon component={GraphIcon} inheritViewBox sx={{width: '30px', height:"30px", mr: 'auto'}}/>
-          <IconButton color='inherit'>{<AccountCircle />}</IconButton>
+          <ButtonGroup variant="text" aria-label="text button group" color='inherit'>
+            <Button color='inherit'>Sign In</Button>
+            <Button color='inherit'>Sign Up</Button>
+          </ButtonGroup>
         </Toolbar>
       </Container>
     </AppBar>
--- src/Pages/Welcome/index.tsx
+++ src/Pages/Welcome/index.tsx
@@ -1,23 +1,26 @@
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { mul, add, reset } from '../../store/counterSlice';
+import { Button, Container, Grid, Typography } from "@mui/material";
+import './index.css';
+import logo from '../../assets/GraphQ-logo.png';
 
 const Welcome = () => {
-  const dispatch = useAppDispatch();
-  const number = useAppSelector((store) => store.counter.number);
+
   return (
     <>
-      <h2>Welcome page content must be here</h2>
-      <h3>redux store work example</h3>
-      <div>Current number: {number}</div>
-      <button type='button' onClick={() => dispatch(add(2))}>
-        add
-      </button>
-      <button type='button' onClick={() => dispatch(mul(3))}>
-        mul
-      </button>
-      <button type='button' onClick={() => dispatch(reset())}>
-        reset
-      </button>
+    <section className="top">
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item xs={8} sx={{mt: 'auto', mb: 'auto'}}>
+            <Typography variant="h3" component="h1" sx={{mb: "30px"}}>GraphiQL App</Typography>
+            <Typography sx={{maxWidth: '700px', mb: "30px"}}>GraphiQL is the GraphQL integrated development environment (IDE). It is a powerful tool you will use often while building bwebsites.    It         offers syntax highlighting, intellisense autocompletion, automatic documentation, and much more. Our team, as part of the final assignment    for the     React     course from the RSSchool, created an application for working with graphQL requests.
+            </Typography>
+            <Button variant="contained">Sing Up</Button>
+          </Grid>
+          <Grid item xs={4}>
+            <img src={logo} alt="graphQL" className="top__logo" loading="lazy"/>
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
     </>
   );
 };
