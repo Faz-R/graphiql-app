44198b27de46b26ef231f3dc8b67632b4bd83593
Faz-R <FazaR92@gmail.com>
2023-05-03 01:36:31 +0300

    feat: add animation for header

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,26 +1,68 @@
 import { GitHub } from "@mui/icons-material";
-import { Container, Link, Paper, SvgIcon, Toolbar, Typography } from "@mui/material";
-import { ReactComponent as Logo } from '../../assets/icon/rs_school.svg';
+import {
+  Container,
+  Link,
+  Paper,
+  SvgIcon,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { ReactComponent as Logo } from "../../assets/icon/rs_school.svg";
 
 const Footer = () => {
   return (
-    <Paper component="footer" color="primary" sx={{flex: "0 0 auto", borderRadius:0, width: '100%', boxShadow: "0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.12)"}} variant="elevation">
-      <Container >
-        <Toolbar sx={{justifyContent: 'space-between'}} disableGutters>
+    <Paper
+      component="footer"
+      color="primary"
+      sx={{
+        flex: "0 0 auto",
+        borderRadius: 0,
+        width: "100%",
+        boxShadow:
+          "0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.12)",
+        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.09), rgba(255, 255, 255, 0.09))`,
+      }}
+      variant="elevation"
+    >
+      <Container>
+        <Toolbar sx={{ justifyContent: "space-between" }} disableGutters>
           <Typography color="#fff">
-            <GitHub color="inherit" fontSize="small" sx={{verticalAlign: 'top'}}></GitHub>
+            <GitHub
+              color="inherit"
+              fontSize="small"
+              sx={{ verticalAlign: "top" }}
+            ></GitHub>
             {` `}
-          <Link href="https://github.com/Faz-R" color="#fff" underline="none">Fazar</Link>
-          {` & `}
-          <Link href="https://github.com/DrabantBY" color="#fff" underline="none">DrabantBY</Link>
-          {` & `}
-          <Link href="https://github.com/sneguroma" color="#fff" underline="none">SneguRoma</Link>
+            <Link href="https://github.com/Faz-R" color="#fff" underline="none">
+              Fazar
+            </Link>
+            {` & `}
+            <Link
+              href="https://github.com/DrabantBY"
+              color="#fff"
+              underline="none"
+            >
+              DrabantBY
+            </Link>
+            {` & `}
+            <Link
+              href="https://github.com/sneguroma"
+              color="#fff"
+              underline="none"
+            >
+              SneguRoma
+            </Link>
           </Typography>
           <Typography color="#fff">2023</Typography>
-          <Link href="https://rs.school/react/" color="#fff" underline="none"><SvgIcon component={Logo} inheritViewBox sx={{width: '60px', mr: 'auto'}}/></Link>
+          <Link href="https://rs.school/react/" color="#fff" underline="none">
+            <SvgIcon
+              component={Logo}
+              inheritViewBox
+              sx={{ width: "60px", mr: "auto" }}
+            />
+          </Link>
         </Toolbar>
       </Container>
-
     </Paper>
   );
 };
