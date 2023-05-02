927a75c3793a3c16a0b364e9d7b488d2b35147be
Faz-R <FazaR92@gmail.com>
2023-05-03 01:36:31 +0300

    feat: add animation for header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,18 +1,55 @@
-import { AppBar, Button, ButtonGroup, Container, SvgIcon, Toolbar } from '@mui/material';
-import { ReactComponent as GraphIcon } from '../../assets/icon/graphql.svg';
-import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Button,
+  ButtonGroup,
+  Container,
+  SvgIcon,
+  Toolbar,
+} from "@mui/material";
+import { ReactComponent as GraphIcon } from "../../assets/icon/graphql.svg";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.onscroll = () => {
+      if (window.scrollY > 0) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+  }, []);
   return (
-    <AppBar position='sticky'>
+    <AppBar position="sticky">
       <Container>
-        <Toolbar disableGutters>
-          <Link to="/" style={{marginRight: 'auto', flex: "0 0 auto"}}>
-            <SvgIcon component={GraphIcon} inheritViewBox sx={{width: '30px', height:"30px", mr: 'auto', flex: "0 0 auto"}}/>
+        <Toolbar
+          disableGutters
+          sx={{
+            height: `${scroll ? `64px` : `80px`}`,
+            transition: `all 0.4s`,
+          }}
+        >
+          <Link to="/" style={{ marginRight: "auto", flex: "0 0 auto" }}>
+            <SvgIcon
+              component={GraphIcon}
+              inheritViewBox
+              sx={{
+                width: "30px",
+                height: "30px",
+                mr: "auto",
+                flex: "0 0 auto",
+              }}
+            />
           </Link>
-          <ButtonGroup variant="text" aria-label="text button group" color='inherit'>
-            <Button color='inherit'>Sign In</Button>
-            <Button color='inherit'>Sign Up</Button>
+          <ButtonGroup
+            variant="text"
+            aria-label="text button group"
+            color="inherit"
+          >
+            <Button color="inherit">Sign In</Button>
+            <Button color="inherit">Sign Up</Button>
           </ButtonGroup>
         </Toolbar>
       </Container>
