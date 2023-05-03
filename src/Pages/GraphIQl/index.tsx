787f07316f6b94fff6391176d31dfde01e7b54d7
Faz-R <FazaR92@gmail.com>
2023-05-03 16:39:33 +0300

    feat: add containers for all pages

--- src/Pages/GraphIQl/index.tsx
+++ src/Pages/GraphIQl/index.tsx
@@ -1,5 +1,11 @@
+import { Container } from "@mui/material";
+
 const GraphIQL = () => {
-  return <h2>The most difficult content must be here</h2>;
+  return (
+    <Container>
+      <h2>The most difficult content must be here</h2>
+    </Container>
+  );
 };
 
 export default GraphIQL;
