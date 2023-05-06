22f3a6e1c7732e2a8d9db238129a3a0ff9b6197d
sneguroma <sneguroma@gmail.com>
2023-05-06 23:44:00 +0200

    feat: add response area

--- src/Components/RequestField/index.tsx
+++ src/Components/RequestField/index.tsx
@@ -1,76 +1,61 @@
-import { IconButton, TextField, Box } from '@mui/material'
-import DirectionsIcon from '@mui/icons-material/Directions'
-import { useState } from 'react'
-import JSONViewer from 'react-json-view';
+import { IconButton, TextField, Box } from "@mui/material";
+import DirectionsIcon from "@mui/icons-material/Directions";
+import { useState } from "react";
 
 function RequestField() {
-  const [request, setRequest] = useState('')
-  const url = 'https://rickandmortyapi.com/graphql';
- const [text, setText] = useState('');
-const [data, setData] = useState(null);
+  const [request, setRequest] = useState("");
+  const url = "https://rickandmortyapi.com/graphql";
+  const [data, setData] = useState("");
 
-  /* useEffect(() => {
-    console.log(text);
-    
-  }, [text]); */
-
-  const resp = async (data = '') => {
+  const resp = async (data = "") => {
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({query: data}),
-    })
-     
-    return await response.json( );    
-  }
+      body: JSON.stringify({ query: data }),
+    });
 
-  const giveAnswer = ()=>{
-    resp(request).then((r) => setData(r) /* setText(JSON.stringify(r)) */);
-    console.log(data);
-  }
+    return await response.json();
+  };
+
+  const giveAnswer = () => {
+    resp(request).then((r: string) => setData(r));
+  };
 
   return (
     <>
       <Box
         component="form"
         sx={{
-          '& .MuiTextField-root': { m: 1, width: '55ch' },
+          "& .MuiTextField-root": { m: 1, width: "55ch" },
         }}
         noValidate
-        autoComplete="off"
-      >
+        autoComplete="off">
         <div>
           <TextField
             id="outlined-multiline-flexible"
             label="enter request"
             multiline
-            rows={10}
+            minRows={10}
             variant="outlined"
             value={request}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setRequest(event.target.value)
+              setRequest(event.target.value);
             }}
           />
           <IconButton
             color="primary"
-            sx={{ p: '10px' }}
+            sx={{ p: "10px" }}
             aria-label="directions"
-            onClick={giveAnswer}
-          >
+            onClick={giveAnswer}>
             <DirectionsIcon />
           </IconButton>
         </div>
       </Box>
-      {data ? (
-        <JSONViewer src={data} />
-      ) : (
-        <p>Loading data...</p>
-      )}
-      <div>{}</div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
-  )
+  );
 }
 
-export default RequestField
+export default RequestField;
