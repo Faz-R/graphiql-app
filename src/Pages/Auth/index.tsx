f653f4c5905f1a8ebe4a857c951b094362f98b11
Faz-R <FazaR92@gmail.com>
2023-05-08 00:57:29 +0300

    feat: add translate for all elements

--- src/Pages/Auth/index.tsx
+++ src/Pages/Auth/index.tsx
@@ -3,9 +3,11 @@ import Form from "../../Components/Form";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { logInWithEmailAndPassword } from "../../firebase";
+import { useTranslation } from "react-i18next";
 
 const SignIn = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const [open, setOpen] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -16,7 +18,7 @@ const SignIn = () => {
       })
       .catch((error: Error) => {
         if (error.message.includes("user-not-found")) {
-          setErrorMessage("User not found");
+          setErrorMessage(`${t("userNotFound")}`);
         } else {
           setErrorMessage(error.message);
         }
@@ -31,7 +33,7 @@ const SignIn = () => {
   return (
     <>
       <Container sx={{ display: "flex", justifyContent: "center" }}>
-        <Form title="Sign In" sendData={handleLogin} isAuth={true} />
+        <Form title={t("signIn")} sendData={handleLogin} isAuth={true} />
         <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
           <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
             {errorMessage}
